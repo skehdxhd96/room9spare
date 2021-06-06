6a5ef78e6a41e8c726c27e5767b2f3f6d65d633c
hsju0202 <hsju0202@gmail.com>
2021-06-06 23:42:53 +0900

    Initial commit

---
+++ src/main/java/com/goomoong/room9backend/Room9BackendApplication.java
@@ -0,0 +1,13 @@
+package com.goomoong.room9backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Room9BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Room9BackendApplication.class, args);
+	}
+
+}
