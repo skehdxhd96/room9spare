d16b67cfa2247c114d0cc2cc51044a963c0f38d8
zooneon <snsxhdz@naver.com>
2021-06-22 00:47:41 +0900

    SETUP: mysql연동

--- src/main/java/com/goomoong/room9backend/Room9BackendApplication.java
+++ src/main/java/com/goomoong/room9backend/Room9BackendApplication.java
@@ -1,6 +1,5 @@
 package com.goomoong.room9backend;
 
-import com.goomoong.room9backend.testClass.DependencyTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Room9BackendApplication {
 
 	public static void main(String[] args) {
-
-		//롬복 테스트
-		DependencyTest dependencyTest = new DependencyTest();
-		dependencyTest.setData("lombok");
-		System.out.println(dependencyTest.getData());
 
 		SpringApplication.run(Room9BackendApplication.class, args);
 	}
---
+++ src/main/java/com/goomoong/room9backend/testClass/DBConnectionRepository.java
@@ -0,0 +1,7 @@
+package com.goomoong.room9backend.testClass;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DBConnectionRepository extends JpaRepository<DBConnection, Long> {
+
+}
