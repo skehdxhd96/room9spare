7a630d212b90e7d3f1c0b3629df5f132e02633a9
hsju0202 <hsju0202@gmail.com>
2021-08-23 16:51:07 +0900

    FIX: 빌드 테스트

--- src/main/java/com/goomoong/room9backend/Room9BackendApplication.java
+++ src/main/java/com/goomoong/room9backend/Room9BackendApplication.java
@@ -10,15 +10,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class Room9BackendApplication {
 
-//	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-//			+ "classpath:application.properties,"
-//			+ "classpath:aws.yml";
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.properties,"
+			+ "classpath:aws.yml";
 
 	public static void main(String[] args) {
-		SpringApplication.run(Room9BackendApplication.class, args);
 
-//		new SpringApplicationBuilder(Room9BackendApplication.class)
-//				.properties(APPLICATION_LOCATIONS)
-//				.run(args);
+		new SpringApplicationBuilder(Room9BackendApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 }
